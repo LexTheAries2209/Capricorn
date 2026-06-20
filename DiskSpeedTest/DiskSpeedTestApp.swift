fa4ae31c052bb4e23815d98d1a6671f948c6ebde
LexTheAries2209 <673661658@qq.com>
2026-06-20 21:23:08 +0900

    Initial Commit

---
+++ DiskSpeedTest/DiskSpeedTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiskSpeedTestApp.swift
+//  DiskSpeedTest
+//
+//  Created by 吴坤城 on 6/20/26.
+//
+
+import SwiftUI
+
+@main
+struct DiskSpeedTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
